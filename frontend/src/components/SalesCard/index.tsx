63829fc3db6e0470453d390f7620fbd509da862e
Perkles <david.goncalves@dcx.ufpb.br>
2022-07-12 15:15:36 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,3 +1,5 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 function SalesCard() {
@@ -6,10 +8,20 @@ function SalesCard() {
       <h2 className="dsmeta-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-space">
-          <input className="dsmeta-form-control" type="text"/>
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
         <div className="dsmeta-form-control-space">
-          <input className="dsmeta-form-control" type="text"/>
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
       <div>
@@ -36,7 +48,7 @@ function SalesCard() {
               <td>
                 <div className="dsmeta-red-botton-container">
                   <div className="dsmeta-red-botton">
-                  <NotificationButton/>
+                    <NotificationButton />
                   </div>
                 </div>
               </td>
@@ -51,7 +63,7 @@ function SalesCard() {
               <td>
                 <div className="dsmeta-red-botton-container">
                   <div className="dsmeta-red-botton">
-                  <NotificationButton/>
+                    <NotificationButton />
                   </div>
                 </div>
               </td>
@@ -65,7 +77,7 @@ function SalesCard() {
               <td>
                 <div className="dsmeta-red-botton-container">
                   <div className="dsmeta-red-botton">
-                    <NotificationButton/>
+                    <NotificationButton />
                   </div>
                 </div>
               </td>
